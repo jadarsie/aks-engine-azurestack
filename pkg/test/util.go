30b39e2928d58894b999805d3aa8c60f95033738
jadarsie <jadarsie@microsoft.com>
2024-04-23 12:55:04 -0700

    test: update test suite to ginkgo v2

--- pkg/test/util.go
+++ pkg/test/util.go
@@ -10,8 +10,8 @@ import (
 
 	"path/filepath"
 
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/reporters"
 	"github.com/onsi/gomega"
 )
 
--- test/e2e/kubernetes/kubernetes_suite_test.go
+++ test/e2e/kubernetes/kubernetes_suite_test.go
@@ -8,8 +8,8 @@ package kubernetes
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
+	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/ginkgo/v2/reporters"
 	. "github.com/onsi/gomega"
 )
 
